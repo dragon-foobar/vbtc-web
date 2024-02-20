a62c18ab413bd139fc3c99afda645b462fc38df4
Dragon Foobar <dragon.foobar@proton.me>
2024-02-20 14:00:48 +1100

    fix bad url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,16 +12,19 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Victorian Bitcoin Technology Club",
     type: "website",
-    url: "https://vbtc.org.au",
-    images:
-      "https://res.cloudinary.com/dgpuwpmjk/image/upload/v1708393812/vbtc-share-2_lgn49h.png",
+    url: new URL("https://vbtc.org.au"),
+    images: new URL(
+      "https://res.cloudinary.com/dgpuwpmjk/image/upload/v1708393812/vbtc-share-2_lgn49h.png"
+    ),
   },
   twitter: {
     card: "summary_large_image",
-    site: "@eMartiiin94",
-    title: "Title webtsite",
-    description: "this is the desciption",
-    images: "url/image.png",
+    site: "@vbtcorg",
+    title: "Victorian Bitcoin Technology Club",
+    description: "Supporting grassroots adoption in Victoria, Australia",
+    images: new URL(
+      "https://res.cloudinary.com/dgpuwpmjk/image/upload/v1708393812/vbtc-share-2_lgn49h.png"
+    ),
   },
 };
 
